4a6fe774655c135d84b9babc536970a5fc183fcc
LoisZheng <632898311@qq.com>
2017-05-18 14:42:29 +0800

    商品查询

--- src/com/yue/controller/ProductController.java
+++ src/com/yue/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yue.model.Product;
 import com.yue.model.Page;
 import com.yue.model.Product;
 import com.yue.service.ProductService;
@@ -24,17 +25,17 @@ public class ProductController {
 	@ResponseBody
 	public JsonResult<Page<Product>> queryProductPageByType(int currentPage, int pageSize,String proType){
 		try{
-			List<Product> getLimit= productService.queryProductPageByType(proType);
+			List<Product> getProduct= productService.queryProductPageByType(proType);
 			Page<Product> page = new Page<Product>();
 			page.setCurrentPage(currentPage);
 			page.setPageSize(pageSize);
-			page.setTotalRow(getLimit.size());
+			page.setTotalRow(getProduct.size());
 			List<Product> pageProduct = new ArrayList<Product>();
 			int start = (page.getCurrentPage()-1)*page.getPageSize();
 			int end = start + page.getPageSize();
 			for(int i=start;i<end;i++){
 				try{
-					pageProduct.add(getLimit.get(i));
+					pageProduct.add(getProduct.get(i));
 				}catch(Exception e){
 					
 				}
@@ -48,6 +49,24 @@ public class ProductController {
 		}catch(Exception e){
 			e.printStackTrace();
 			return new JsonResult<Page<Product>>(e);
+		}
+		
+	}
+	
+//	根据权限编码查询商品
+	@RequestMapping(value="queryProductById")
+	@ResponseBody
+	public JsonResult<Product> queryProductById(String proId){
+		try{
+			Product getProduct= productService.queryProductById(proId);
+			try{
+				return new JsonResult<Product>(getProduct);
+			}catch(Exception e){
+				return new JsonResult<Product>(e);
+			}		
+		}catch(Exception e){
+			e.printStackTrace();
+			return new JsonResult<Product>(e);
 		}
 		
 	}
